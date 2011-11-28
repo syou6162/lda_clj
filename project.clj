7e44e18181381eeac34010045c855cd5db55c2cb
Yasuhisa Yoshida <syou6162@gmail.com>
2011-11-28 10:06:18 +0900

    clojure-contribを依存関係に追加

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject lda_clj "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [org.clojure/clojure-contrib "1.3.0"]])
