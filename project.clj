6daee99b7f8b4fe37b4d384bc96370e0dbe1e6af
Yasuhisa Yoshida <syou6162@gmail.com>
2011-12-03 07:05:47 +0900

    testが動くようにとりあえずバージョンを変更

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject lda_clj "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-		 [org.clojure/clojure-contrib "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 ;; [org.clojure/clojure "1.3.0"]
+		 ;; [org.clojure/clojure-contrib "1.3.0"]
+		 ]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  ;; :repositories {"clojars" "http://clojars.org/repo"}
+  )
