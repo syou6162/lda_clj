9b13095743d02696ce54147f3481ea67fa9775dc
Yasuhisa Yoshida <syou6162@gmail.com>
2011-12-22 08:44:30 +0900

    import-staticを使って長い関数名(logGamma)を簡単に書けるようにした

--- src/lda_clj/log_likelihood.clj
+++ src/lda_clj/log_likelihood.clj
@@ -1,28 +1,29 @@
 (ns lda_clj.log_likelihood
-  (:import (org.apache.commons.math.special.Gamma.logGamma))
   (:use [lda_clj.corpora])
   (:use [lda_clj.util])
   (:use [lda_clj.document]))
 
+(use '[clojure.contrib.import-static :only (import-static)])
+(import-static org.apache.commons.math.special.Gamma logGamma)
+
 (defn ^Double calc-prior-term [documents]
   (let [N (count documents)]
-    (+ (- (* N (org.apache.commons.math.special.Gamma/logGamma (* @alpha @K)))
-	  (* (* N @K) (org.apache.commons.math.special.Gamma/logGamma @alpha)))
+    (+ (- (* N (logGamma (* @alpha @K)))
+	  (* (* N @K) (logGamma @alpha)))
        (reduce + (for [d documents]
 		   (- (reduce + (for [z (range @K)]
-				  (org.apache.commons.math.special.Gamma/logGamma (+ ((d :Nz) z) @alpha))))
-		      (org.apache.commons.math.special.Gamma/logGamma (+ (count (d :w)) (* @alpha @K)))))))))
+				  (logGamma (+ ((d :Nz) z) @alpha))))
+		      (logGamma (+ (count (d :w)) (* @alpha @K)))))))))
 
 (defn ^Double calc-likelihood-term [corpora]
   (let [V (corpora :V)]
-    (+ (- (* @K (org.apache.commons.math.special.Gamma/logGamma (* @beta V)))
-	  (* (* @K V) (org.apache.commons.math.special.Gamma/logGamma @beta)))
+    (+ (- (* @K (logGamma (* @beta V)))
+	  (* (* @K V) (logGamma @beta)))
      (reduce + (for [z (range @K)]
 		 (- (reduce + (for [v (range V)]
-				(org.apache.commons.math.special.Gamma/logGamma (+ (((corpora :Nzw) z) v) @beta))))
-		    (org.apache.commons.math.special.Gamma/logGamma (+ ((corpora :Nz) z) (* @beta V)))))))))
+				(logGamma (+ (((corpora :Nzw) z) v) @beta))))
+		    (logGamma (+ ((corpora :Nz) z) (* @beta V)))))))))
 
 (defn ^Double log-likelihood [corpora]
   (+ (calc-prior-term (corpora :documents))
      (calc-likelihood-term corpora)))
-
