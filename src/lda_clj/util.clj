56a4fc4ee0d2914ac23f04130426be4d95162b19
Yasuhisa Yoshida <syou6162@gmail.com>
2012-02-14 21:10:49 +0900

    wordからidに変換する関数を変更

--- src/lda_clj/util.clj
+++ src/lda_clj/util.clj
@@ -277,20 +277,8 @@
 ;;      )
 ;;     ))
 
-(defn get-word-id [vec]
-  (let [[word2id-map word] vec
-	word-id (word2id-map word)
-	new-word-id (count word2id-map)]
-    (if word-id
-      [word2id-map word-id]
-      [(assoc word2id-map word new-word-id) new-word-id])))
+(defn get-word-id [word2id word]
+  (assoc word2id word (get word2id word (count word2id))))
 
-(defn get-words-ids [word2id-map words]
-  (second (reduce (fn [result word]
-		    (let [word2id-map (first result)
-			  words-ids (second result)
-			  tmp-result (get-word-id [word2id-map word])
-			  word2id-map (first tmp-result)
-			  new-word-id (second tmp-result)]
-		      [(first tmp-result) (conj words-ids new-word-id)]))
-		  [word2id-map []] words)))
+(defn get-words-ids [word2id words]
+  (reduce (fn [m w] (get-word-id m w)) word2id words))
