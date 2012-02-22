7f396689bd214049b8b2a853a1d7d95a19928710
Yasuhisa Yoshida <syou6162@gmail.com>
2012-02-22 21:10:12 +0900

    依存関係の追加

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
 		 [org.apache.commons/commons-math "2.2"]
+		 [org.danlarkin/clojure-json "1.2-SNAPSHOT"]
 		 [clojure-opennlp "0.1.7"]]
   :dev-dependencies [[swank-clojure "1.3.2"]]
   :main lda_clj.core)
