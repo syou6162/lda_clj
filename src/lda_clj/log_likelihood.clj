ffac6daeba3483b640526599bc519db3e0c19ce2
Yasuhisa Yoshida <syou6162@gmail.com>
2012-08-20 23:40:42 +0900

    sumマクロを使って見通しがしやすいように変更

--- src/lda_clj/log_likelihood.clj
+++ src/lda_clj/log_likelihood.clj
@@ -7,26 +7,29 @@
 (defn logGamma [x]
   (org.apache.commons.math.special.Gamma/logGamma x))
 
+(defmacro sum [bindings expr]
+  `(reduce + (for ~bindings ~expr)))
+
 (defn ^Double calc-prior-term [corp alpha]
   (let [documents (corp :documents)
 	N (count documents)
 	K (corp :K)]
     (+ (- (* N (logGamma (* alpha K)))
 	  (* (* N K) (logGamma alpha)))
-       (reduce + (for [d documents]
-		   (- (reduce + (for [z (range K)]
-				  (logGamma (+ ((d :Nz) z) alpha))))
-		      (logGamma (+ (count (d :w)) (* alpha K)))))))))
+       (sum [d documents]
+	    (- (sum [z (range K)]
+		    (logGamma (+ ((d :Nz) z) alpha)))
+	       (logGamma (+ (count (d :w)) (* alpha K))))))))
 
 (defn ^Double calc-likelihood-term [corp beta]
   (let [V (corp :V)
 	K (corp :K)]
     (+ (- (* K (logGamma (* beta V)))
 	  (* (* K V) (logGamma beta)))
-       (reduce + (for [z (range K)]
-		   (- (reduce + (for [v (range V)]
-				  (logGamma (+ (get-in corp [:Nzw z v]) beta))))
-		      (logGamma (+ (get-in corp [:Nz z]) (* beta V)))))))))
+       (sum [z (range K)]
+	    (- (sum [v (range V)]
+		    (logGamma (+ (get-in corp [:Nzw z v]) beta)))
+	       (logGamma (+ (get-in corp [:Nz z]) (* beta V))))))))
 
 (defn ^Double log-likelihood [corp alpha beta]
   (+ (calc-prior-term corp alpha)
